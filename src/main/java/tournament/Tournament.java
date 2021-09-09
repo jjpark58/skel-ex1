174785f770354c82ffc5ff8ca42618a2b28dae87
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2021-09-09 06:12:14 -0700

    initialized repo

---
+++ src/main/java/tournament/Tournament.java
@@ -0,0 +1,17 @@
+package tournament;
+
+public class Tournament {
+
+    /**
+     * @param scorecard the scores in the tournament and is not null; {@code scoredcard[i][j]} represents the score of Player i in Game j. Each row in this array has {@code sets * games} entries.
+     * @param sets the number of sets played in the tournament
+     * @param games the number of games per set
+     * @return a list of players in order of their performance (best to worst)
+     */
+    public static int[] rankPlayers(int[][] scorecard, int sets, int games) {
+        int[] playerRankings;
+        // TODO: Your implementation goes here
+        return null;
+    }
+    
+}
